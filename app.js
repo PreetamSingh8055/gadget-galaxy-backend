f6903549bba8659469f2943dcbd3ba6d7478c7a1
PreetamSingh8055 <thepreet999@gmail.com>
2026-01-28 11:10:59 +0530

    fix cors for production

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173",
-              "http://localhost:5174", // admin (optional)
-       "https://gadget-dukan.vercel.app"],
+    origin:
+      [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://gadget-galaxy-client.netlify.app",
+        "https://gadget-galaxy-admin.netlify.app"
+      ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
